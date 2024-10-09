5548082341b2fb4622c21fc947b0d95ed1f3a7a3
1etterh <1etterh.dev@gmail.com>
2024-10-09 15:38:56 +0900

    Add: UserTable Insertion

---
+++ data_insertion/user.sql
@@ -0,0 +1,10 @@
+INSERT INTO tbl_user (user_name, password, nickname, email, profile_image, created_at, signup_path, user_identifier, user_auth_id)
+VALUES
+    ('김슈슈', 'pw1234', 'shushu_ping', 'siusiu@example.com', NULL, '2023-08-01 10:30:00', 'NORMAL', CONCAT('NORMAL_', 'siusiu_ping'), 'siusiu_ping'),  -- user_identifier는 'NORMAL_alice123', user_auth_id는 사용자가 입력한 ID
+    ('이효효', NULL, 'hyohyo', 'jinyjin@example.com', NULL, '2023-08-02 11:45:00', 'NAVER', CONCAT('NAVER_', '987654321'), '987654321'),  -- user_identifier는 'NAVER_987654321', user_auth_id는 네이버의 고유번호 예시
+    ('김서서', NULL, 'seoseo', 'hyunhyun@example.com', NULL, '2023-08-03 12:00:00', 'GOOGLE', CONCAT('GOOGLE_', 'abc123xyz'), 'abc123xyz'),  -- user_identifier는 'GOOGLE_abc123xyz', user_auth_id는 구글의 고유번호 예시
+    ('이우우', 'password123', 'woowoo', 'woowoo@example.com', NULL, '2023-08-04 09:15:00', 'NORMAL', CONCAT('NORMAL_', 'woowoo'), 'woowoo'),  -- user_identifier는 'NORMAL_diana789', user_auth_id는 사용자가 입력한 ID
+    ('김도옹', 'pwpw321','do_ong','doong@example.com',NULL, '2023-08-01 09:15:00','NORMAL',CONCAT('NORMAL_','do_ong'),'do-ong'),
+    ('김모모', NULL, 'momo-ping', 'momo_ping@example.com', NULL, '2023-08-05 14:20:00', 'KAKAO', CONCAT('KAKAO_', 'kakao98765'), 'kakao98765');  -- user_identifier는 'KAKAO_kakao98765', user_auth_id는 카카오의 고유번호 예시
+use mudiumdb;
+
--- table_definition/table_definition.sql
+++ table_definition/table_definition.sql
@@ -1,18 +1,23 @@
--- USER 테이블
+
+-- 외래키 제약 조건 비활성화
+DROP DATABASE IF EXISTS mudiumdb;
+CREATE DATABASE MUDIUMDB;
+USE MUDIUMDB;
+
 CREATE TABLE TBL_USER (
-                      user_id BIGINT(20) NOT NULL AUTO_INCREMENT,
-                      user_auth_id VARCHAR(255) NOT NULL,  -- 일반 로그인 ID 또는 소셜 로그인 고유번호
-                      user_name VARCHAR(255) NOT NULL,
-                      password VARCHAR(255),
-                      nickname VARCHAR(255),
-                      email VARCHAR(255),
-                      user_status VARCHAR(255) NOT NULL DEFAULT 'ACTIVE' CHECK(user_status IN ('ACTIVE','INACTIVE')),
-                      created_at TIMESTAMP,
-                      withdrawn_at TIMESTAMP,
-                      profile_image TEXT,
-                      accept_status VARCHAR(255) NOT NULL DEFAULT 'Y' CHECK(accept_status IN ('Y','N')),
-                      signup_path VARCHAR(255) CHECK (signup_path IN ('KAKAO','NAVER','GOOGLE','NORMAL')),  -- 가입 경로는 KAKAO,NAVER,GOOGLE,NORMAL
-                      user_identifier VARCHAR(511) NOT NULL,  -- 신규 추가, 가입 경로 + ID(or 고유번호)
-                      PRIMARY KEY (user_id),
-                      UNIQUE KEY unique_user_identifier (user_identifier)  -- 유니크 인덱스 추가
+                          user_id BIGINT(20) NOT NULL AUTO_INCREMENT,
+                          user_auth_id VARCHAR(255) NOT NULL,  -- 일반 로그인 ID 또는 소셜 로그인 고유번호
+                          user_name VARCHAR(255) NOT NULL,
+                          password VARCHAR(255),
+                          nickname VARCHAR(255),
+                          email VARCHAR(255),
+                          user_status VARCHAR(255) NOT NULL DEFAULT 'ACTIVE' CHECK(user_status IN ('ACTIVE','INACTIVE')),
+                          created_at TIMESTAMP,
+                          withdrawn_at TIMESTAMP,
+                          profile_image TEXT,
+                          accept_status VARCHAR(255) NOT NULL DEFAULT 'N' CHECK(accept_status IN ('Y','N')),
+                          signup_path VARCHAR(255) CHECK (signup_path IN ('KAKAO','NAVER','GOOGLE','NORMAL')),  -- 가입 경로는 KAKAO,NAVER,GOOGLE,NORMAL
+                          user_identifier VARCHAR(511) NOT NULL,  -- 신규 추가, 가입 경로 + ID(or 고유번호)
+                          PRIMARY KEY (user_id),
+                          UNIQUE KEY unique_user_identifier (user_identifier)  -- 유니크 인덱스 추가
 ) ENGINE=INNODB AUTO_INCREMENT=1 COMMENT='회원' DEFAULT CHARSET=UTF8;
