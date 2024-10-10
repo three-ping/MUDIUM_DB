72fb44f1b043848eb8fb9d125c6a07dbbbc01f94
이우진 <147306997+Vorschlag-bit@users.noreply.github.com>
2024-10-10 17:50:59 +0900

    Create data_musicalInfo.sql
    뮤지컬 더미 데이터를 생성했습니다.
    실제로 요청 성공 시 진짜 정보들로 바꾸겠습니다.

---
+++ data_insertion/data_musicalInfo.sql
@@ -0,0 +1,11 @@
+INSERT INTO `TBL_MUSICAL_INFO` (`title`, `image_url`, `rating`, `view_count`, `review_video`) VALUES
+('잠자는 숲속의 공주', 'http://www.kopis.or.kr/upload/pfmPoster/PF_PF239321_240416_114951.JPG', '전체 관람가', 1500, 'https://www.youtube.com/watch?v=dummy1'),
+('오페라의 유령', 'http://www.kopis.or.kr/upload/pfmPoster/PF_PF239322_240417_120000.JPG', '12세 이상', 5000, 'https://www.youtube.com/watch?v=dummy2'),
+('라이온 킹', 'http://www.kopis.or.kr/upload/pfmPoster/PF_PF239323_240418_130000.JPG', '전체 관람가', 3200, 'https://www.youtube.com/watch?v=dummy3'),
+('위키드', 'http://www.kopis.or.kr/upload/pfmPoster/PF_PF239324_240419_140000.JPG', '8세 이상', 4100, 'https://www.youtube.com/watch?v=dummy4'),
+('맘마미아!', 'http://www.kopis.or.kr/upload/pfmPoster/PF_PF239325_240420_150000.JPG', '전체 관람가', 2800, 'https://www.youtube.com/watch?v=dummy5'),
+('캣츠', 'http://www.kopis.or.kr/upload/pfmPoster/PF_PF239326_240421_160000.JPG', '전체 관람가', 3800, 'https://www.youtube.com/watch?v=dummy6'),
+('레미제라블', 'http://www.kopis.or.kr/upload/pfmPoster/PF_PF239327_240422_170000.JPG', '12세 이상', 4500, 'https://www.youtube.com/watch?v=dummy7'),
+('시카고', 'http://www.kopis.or.kr/upload/pfmPoster/PF_PF239328_240423_180000.JPG', '15세 이상', 3000, 'https://www.youtube.com/watch?v=dummy8'),
+('아이다', 'http://www.kopis.or.kr/upload/pfmPoster/PF_PF239329_240424_190000.JPG', '전체 관람가', 2200, 'https://www.youtube.com/watch?v=dummy9'),
+('노트르담 드 파리', 'http://www.kopis.or.kr/upload/pfmPoster/PF_PF239330_240425_200000.JPG', '12세 이상', 3500, 'https://www.youtube.com/watch?v=dummy10');
