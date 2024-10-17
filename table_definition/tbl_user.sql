5fc42efbdf53a6598227d09d866a7833116666b6
1etterh <1etterh.dev@gmail.com>
2024-10-17 17:05:29 +0900

    refactor: 유저 아이디 삭제, 아이디는 이메일로 함

--- table_definition/tbl_user.sql
+++ table_definition/tbl_user.sql
@@ -5,7 +5,6 @@ USE MUDIUMDB;
 
 CREATE TABLE TBL_USER (
                           user_id BIGINT(20) NOT NULL AUTO_INCREMENT,
-                          user_auth_id VARCHAR(255) NOT NULL,  -- 일반 로그인 ID 또는 소셜 로그인 고유번호
                           user_name VARCHAR(255) NOT NULL,
                           password VARCHAR(255),
                           nickname VARCHAR(255),
